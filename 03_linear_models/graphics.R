b1cdb990798e0d269eae9c8bbb7656ff578f7ec6
saramortara <saramortara@gmail.com>
2024-02-01 00:26:24 -0300

    model diagnostics now w/ linear models tutorial

--- 03_linear_models/graphics.R
+++ 03_linear_models/graphics.R
@@ -41,3 +41,18 @@ p_expect <- ggplot() +
 p_expect
 ggsave("03_linear_models/figs/expect0.png", bg = "transparent", width = 6, height = 6)
 
+plot(0, 0, type = "n", bty = "l", las = 1, 
+     xaxt = "n", yaxt = "n", xlab = "X", ylab  = "Y")
+abline(a = 0, b = 0.7)
+segments(x0 = -0.42, y0 = -0.3, x1 = 0.13, y1 = -0.3)
+segments(x0 = 0.13, y0 = -0.3, x1 = 0.13, y1 = .09)
+text(x = 0.2, y = -.1, '}', cex = 4)
+text(x = 0.33, y = -.1, expression(paste(beta, 1)), cex = 2)
+arrows(x0 = -0.42, y0 = -0.4, x1 = 0.13, y1 = -0.4, angle = 90, 
+       length = 0.05, code = 3)
+text(x = -.145, y = -0.45, "1")
+text(x = -1, y = -.89, '}', cex = 3.7)
+text(x = -0.85, y = -.89, expression(paste(beta, "0")), cex = 2)
+
+
+read.csv("03_linear_models/crawley_regression.csv")
