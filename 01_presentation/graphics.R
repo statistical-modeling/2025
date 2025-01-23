df655b36819ee1ab48bc9a7fcb73d54fe4bfe0ff
saramortara <saramortara@gmail.com>
2025-01-23 12:30:46 -0300

    updating aula 1, changing example and adding rethinking

--- 01_presentation/graphics.R
+++ 01_presentation/graphics.R
@@ -1,177 +1,171 @@
-# Graphics for presentation
-
+# Load the necessary libraries
+library(rethinking)
 library(ggplot2)
+library(dplyr)
+library(readr)
 
-## ggplot theme
-my_theme <- theme(
-     panel.background = element_rect(fill='transparent'),
-     plot.background = element_rect(fill='transparent', color=NA),
-     #panel.grid.major = element_blank(),
-     #panel.grid.minor = element_blank(),
-     legend.background = element_rect(fill='transparent', color = NA),
-     legend.box.background = element_rect(fill='transparent', color = NA),
-     axis.title = element_text(face = "bold",size = rel(1.2)),
-     axis.title.y = element_text(angle=90,vjust =2),
-     axis.title.x = element_text(vjust = -0.2),
-     axis.line = element_line(colour="black"),
-     axis.ticks = element_line(),
-     axis.text.x = element_blank(),
-     axis.ticks.x = element_blank(),
-     axis.text.y = element_blank(),
-     axis.ticks.y = element_blank(),
-     legend.key = element_rect(colour = NA),
-     legend.position = "bottom",
-     legend.direction = "horizontal",
-     legend.key.size= unit(0.2, "cm")
-)
-
-## Data
-
-
-area <- c(321000, 290000, 144000, 70000, 48000, 25000, 4500, 8870, 18000, 4600)
-species <- c(540, 420, 368, 220, 337, 232, 111, 143, 137, 108)
+area <- sort(c(321000, 290000, 144000, 70000, 48000, 25000, 4500, 8870, 18000, 4600))
+species <- c(540, 420, 368, 220, 337, 232, 131, 143, 137, 108)
 island <- c("New \nGuinea", "Borneo", "Phillipines", "Celebes", 
             "Java", "Ceylon", "Palawan", "Flores", "Timor", "Sumba")
 
 
-df <- data.frame(area = area, 
+fake_df <- data.frame(area = area, 
                  species = species, 
                  island = island)
 
 
-# Theory -----------------------------------------------------------------------
-set.seed(1492)
+## ggplot theme
+my_theme <- theme(
+  panel.background = element_rect(fill='transparent'),
+  plot.background = element_rect(fill='transparent', color=NA),
+  #panel.grid.major = element_blank(),
+  #panel.grid.minor = element_blank(),
+  legend.background = element_rect(fill='transparent', color = NA),
+  legend.box.background = element_rect(fill='transparent', color = NA),
+  axis.title = element_text(face = "bold",size = rel(1.2)),
+  axis.title.y = element_text(angle=90,vjust =2),
+  axis.title.x = element_text(vjust = -0.2),
+  axis.line = element_line(colour="black"),
+  axis.ticks = element_line(),
+  axis.text.x = element_blank(),
+  axis.ticks.x = element_blank(),
+  axis.text.y = element_blank(),
+  axis.ticks.y = element_blank(),
+  legend.key = element_rect(colour = NA),
+  legend.position = "bottom",
+  legend.direction = "horizontal",
+  legend.key.size= unit(0.2, "cm")
+)
 
 
-p_theory <- ggplot() +
-  xlim(-5, 5) +
-  geom_function(aes(colour = "Extinction"), fun = dnorm, args = list(mean = 90, sd = 18.5), lwd = 1) +
-  geom_function(aes(colour = "Extinction"), fun = dnorm, args = list(mean = 90, sd = 19), lwd = 1) +
-  geom_function(aes(colour = "Immigration"), fun = dnorm, args = list(mean = -90, sd = 19), lwd = 1) +
-  labs(colour = "", x = "Number of species", y = "Rate") +
-  annotate("text", x = 1.5, y = 5e-7, label = "Small", size = 5) +
-  annotate("text", x = 1.5, y = 3e-7, label = "Large", size = 5) +
-  my_theme
-  
 
-p_theory
-ggsave("01_presentation/figs/theory.png", bg = "transparent", width = 6, height = 6)
+df <- read_delim("01_presentation/BiomassDataset.csv",
+                   delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ","),
+                   trim_ws = TRUE)
 
-# Expect -----------------------------------------------------------------------
 
-p_expect <- ggplot() +
-  geom_abline(slope = 0.6, intercept = 0.1, lwd = 1.5) +
-  my_theme +
-  labs(x = "Area", y = "Number of species") +
-  theme(
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
-  ) + my_theme
+# Define the model
+model <- alist(
+  Biomass ~ dnorm(mu, sigma),
+  mu <- a + b * AGCWP,
+  a ~ dnorm(10, 5),  # Prior for intercept
+  b ~ dnorm(0, 1),   # Prior for slope
+  sigma ~ dnorm(0, 1)    # Prior for residual SD
+)
 
-p_expect
-ggsave("01_presentation/figs/expect.png", bg = "transparent", width = 6, height = 6)
+# Fit the model
+fit <- quap(
+  model,
+  data = df
+)
 
-# Plot data --------------------------------------------------------------------
-data_plot <- ggplot(data = df, aes(x = area, y = species)) +
-  geom_point() +
-  labs(x = "Area", y = "Species") +
-  #scale_y_continuous(trans = "log10") +
-  scale_x_continuous(labels = scales::comma) +
-  #geom_smooth(method = "lm", color = "#586e75", fill = "#586e75", alpha = .2) +
-  geom_text(label = island, vjust = 1.5) +
+# Extract posterior samples
+post <- extract.samples(fit)
+
+# Generate posterior predictions for a sequence of AGCWP values
+AGCWP_seq <- seq(min(df$AGCWP), max(df$AGCWP), length = 100)
+mu_pred <- sapply(AGCWP_seq, function(x) post$a + post$b * x)
+
+# Calculate mean and credible intervals
+mu_df <- data.frame(
+  AGCWP = AGCWP_seq,
+  mean = apply(mu_pred, 2, mean),
+  lower = apply(mu_pred, 2, PI, prob = 0.999)[1, ],
+  upper = apply(mu_pred, 2, PI, prob = 0.999)[2, ]
+)
+
+# Combine datasets for ggplot compatibility
+data_combined <- df %>% 
+  rename(mean = Biomass) %>% 
+  mutate(lower = NA, upper = NA)
+
+mu_df$source <- "Model"
+data_combined$source <- "Observed"
+
+final_data <- bind_rows(mu_df, data_combined)
+
+# Data plot --------------------------------------------------------------------------
+data_plot <- ggplot() +
+  geom_point(data = df, aes(x = AGCWP, y = Biomass), color = "forestgreen", size = 3) +
+  labs(
+    x = "AGCWP (Mg C ha^-1 yr^-1)",
+    y = "Biomass (Mg DW ha^-1 / Basal area)"
+  ) +
   my_theme
 
 data_plot
 ggsave("01_presentation/figs/data_plot.png", bg = "transparent", width = 6, height = 6)
 
-data_plot2 <- ggplot(data = df, aes(x = area, y = species)) +
-  geom_point() +
-  labs(x = "Area (log)", y = "Species (log)") +
-  scale_y_continuous(trans = "log10") +
-  scale_x_continuous(trans = "log10", labels = scales::comma) +
-  #geom_smooth(method = "lm", color = "#586e75", fill = "#586e75", alpha = .2) +
-  geom_text(label = island, vjust = 1.5) +
+
+
+# Fit --------------------------------------------------------------------------
+fit <- ggplot() +
+  geom_point(data = df, aes(x = AGCWP, y = Biomass), color = "forestgreen", size = 3) +
+  geom_line(data = mu_df, aes(x = AGCWP, y = mean), color = "black") +
+  geom_ribbon(data = mu_df, aes(x = AGCWP, ymin = lower, ymax = upper), fill = "gray", alpha = 0.5) +
+  labs(
+    x = "AGCWP (Mg C ha^-1 yr^-1)",
+    y = "Biomass (Mg DW ha^-1 / Basal area)"
+  ) +
   my_theme
 
-
-data_plot2
-ggsave("01_presentation/figs/data_plot2.png", bg = "transparent", width = 6, height = 6)
-
+fit
+ggsave("01_presentation/figs/fit.png", bg = "transparent", width = 6, height = 6)
 
 # Expect variation -------------------------------------------------------------
-expect_variation <- ggplot(data = df, aes(x = area, y = species)) +
+expect_variation <- ggplot(data = fake_df, aes(x = area, y = species)) +
   xlim(0, max(df$area + 100000)) +
-  geom_point() +
-  labs(x = "Area", y = "Species") +
   scale_y_continuous(trans = "log10") +
   scale_x_continuous(trans = "log10", labels = scales::comma) +
+  geom_point() +
+  labs(x = "Wood productivity", y = "Biomass") +
   geom_smooth(method = "gam",  level = 0.9995,
               color = "#586e75", fill = "#586e75", alpha = .2) + 
-  annotate("text", x = 50000, y = 240, label= "Expected", color="#586e75", size=7, angle = 36) +
-  annotate("text", x = 3900, y = 110, label= "Variation", color="#586e75", size=7, angle = 90) +
+  annotate("text", x = 45000, y = 180, label= "Expected", color="#586e75", 
+           size=7, angle = -40) +
+  annotate("text", x = 3900, y = 420, label= "Variation", color="#586e75", 
+           size=7, angle = 90) +
   my_theme
 
 expect_variation
 ggsave("01_presentation/figs/expect_variation.png", bg = "transparent", width = 6, height = 6)
 
-# Dnorm ------------------------------------------------------------------------
-x1 <- seq(-3.5,3.5, by=0.001)
-
-df2 <- data.frame(
-  X = x1,
-  Y = dnorm(x1)
-) 
-
-p.g <-
-  ggplot(df2, aes(X,Y)) +
-  geom_line() +
-  theme_void() +
-  theme(legend.position = "none") +
-  geom_segment(aes(xend=X, yend=0, colour = "#586e75", alpha = .3)) +
-  geom_segment(aes(x=0, y=dnorm(0), xend=0, yend=0), color="#586e75")
-
-p.g + annotate("text", x = 0, y = 0.42, label= "Expected", color="#586e75", size=10) +
-  annotate("segment", x = -3.6, xend = 3.6, y = -0.015, yend = -0.015,
-           colour = "#586e75", linewidth = 1.25) +
-  annotate("text", x = 3.5, y = -0.05, label= "Y", color="#586e75", size=18)
-
-ggsave("01_presentation/figs/dnorm.png", bg = "transparent", width = 6, height = 6)
-
-
-# Dnorm2 -----------------------------------------------------------------------
-p.g +
-    annotate("text", x = 0, y = 0.42, label= "mu", size=15, parse=TRUE) +
-    annotate("text", x = 0.5, y = 0.17, label= "sigma", size=15, parse=TRUE) +
-    annotate("segment", x = -0.5, xend = 0.5, y = .15, yend = .15,
-             colour = "black", linewidth = 1.5, arrow = arrow(ends="both")) +
-    annotate("segment", x = -3.6, xend = 3.6, y = -0.015, yend = -0.015,
-             colour = "#586e75", size = 1.25) +
-    annotate("text", x = 3.5, y = -0.05, label= "Y", color="#586e75", size=18)
-
-ggsave("01_presentation/figs/dnorm2.png", bg = "transparent", width = 6, height = 6)
 
 # Expect var2 ------------------------------------------------------------------
-ggplot(data = df, aes(x = area, y = species)) +
+ggplot(data = fake_df, aes(x = area, y = species)) +
   xlim(0, max(df$area + 100000)) +
   geom_point() +
-  labs(x = "Area (log)", y = "Species (log)") +
+  labs(x = "Wood productivity", y = "Biomass") +
   scale_y_continuous(trans = "log10") +
   scale_x_continuous(trans = "log10", labels = scales::comma) +
   geom_smooth(method = "gam",  level = 0.9995,
               color = "#586e75", fill = "#586e75", alpha = .2) + 
-  annotate("text", x = 50000, y = 240, label= "mu", color="#586e75", size=7, angle = 36, parse = TRUE) +
-  annotate("text", x = 3900, y = 110, label= "sigma", color="#586e75", size=7, angle = 90, parse = TRUE) +
+  annotate("text", x = 45000, y = 180, label= "mu", color="#586e75", size=7, angle = -40, parse = TRUE) +
+  annotate("text", x = 3900, y =420, label= "sigma", color="#586e75", size=7, angle = 90, parse = TRUE) +
   my_theme
 
 ggsave("01_presentation/figs/expect_variation2.png", bg = "transparent", width = 6, height = 6)
 
-# Theory 2 ---------------------------------------------------------------------
+
+# Expect var3 ------------------------------------------------------------------
+ggplot(data = fake_df, aes(x = area, y = species)) +
+  xlim(0, max(df$area + 100000)) +
+  geom_point() +
+  labs(x = "Wood productivity", y = "Biomass") +
+  scale_y_continuous(trans = "log10") +
+  scale_x_continuous(trans = "log10", labels = scales::comma) +
+  geom_smooth(method = "gam",  level = 0.9995,
+              color = "#586e75", fill = "#586e75", alpha = .2) + 
+  my_theme
+
+ggsave("01_presentation/figs/expect_variation3.png", bg = "transparent", width = 6, height = 6)
+
+
+# Theory -----------------------------------------------------------------------
 ggplot() +
   geom_abline(slope = 0.6, intercept = 0.1, lwd = 1.5) +
-  theme_Publication() +
-  labs(x = "Area (log)", y = "Number of species (log)") +
+  labs(x = "Wood productivity", y = "Biomass") +
   theme(
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
@@ -179,45 +173,33 @@ ggplot() +
     axis.ticks.y = element_blank()
   ) + my_theme
 
-ggsave("01_presentation/figs/theory_log.png", bg = "transparent", width = 6, height = 6)
+ggsave("01_presentation/figs/theory1.png", bg = "transparent", width = 6, height = 6)
 
+# Theory alt -------------------------------------------------------------------
+ggplot() +
+  geom_abline(slope = -0.05, intercept = .525, lwd = 1.5) +
+  geom_hline(yintercept = .5, lwd = 1.5) +
+  labs(x = "Wood productivity", y = "Biomass") +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) + my_theme
 
-# Theory 3----------------------------------------------------------------------
-ggplot(data = df, aes(x = area, y = species)) +
-  labs(x = "Area (log)", y = "Species (log)") +
-  scale_y_continuous(trans = "log10") +
-  scale_x_continuous(trans = "log10", labels = scales::comma) +
-  geom_smooth(method = "gam",  level = 0.9995,
-              color = "#586e75", fill = "#586e75", alpha = .2) + 
-  my_theme
+ggsave("01_presentation/figs/theory2.png", bg = "transparent", width = 6, height = 6)
+
+# Theory alt2 -------------------------------------------------------------------
+ggplot() +
+  geom_abline(slope = -0.6, intercept = .7, lwd = 1.5) +
+  #geom_hline(yintercept = .5, lwd = 1.5) +
+  labs(x = "Wood productivity", y = "Biomass") +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) + my_theme
 
 ggsave("01_presentation/figs/theory3.png", bg = "transparent", width = 6, height = 6)
-
-# Fit --------------------------------------------------------------------------
-p_fit <- ggplot(data = df, aes(x = area, y = species)) +
-  geom_point() +
-  labs(x = "Area (log)", y = "Species (log)") +
-  scale_y_continuous(trans = "log10") +
-  scale_x_continuous(trans = "log10", labels = scales::comma) +
-  geom_smooth(method = "lm",  color = "#586e75", se = FALSE) + 
-  my_theme
-
-p_fit
-ggsave("01_presentation/figs/fit.png", bg = "transparent", width = 6, height = 6)
-
-
-
-pred <- predict(mod)
-
-log_c <-  coef(mod)[1]
-z <- coef(mod)[2]
-
-log_c + z * log(df$area)
-
-
-coef(mod)[1] + coef(mod)[2] * log(df$area)
-pred
-
-S <- exp(log_c) * df$area ^ z
-S
 
